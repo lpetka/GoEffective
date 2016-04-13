b23f895517acaab217067eec6d7bb7e08623a379
Łukasz Petka <petka@student.agh.edu.pl>
2016-04-13 22:37:02 +0200

    upgrade gradle and add BaseActivity class

---
+++ app/src/main/java/project/io/goeffective/activities/BaseActivity.java
@@ -0,0 +1,52 @@
+package project.io.goeffective.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import project.io.goeffective.presenters.IPresenter;
+import rx.subscriptions.CompositeSubscription;
+
+public abstract class BaseActivity extends Activity {
+    protected IPresenter presenter;
+    private final int layoutResID;
+
+    protected final CompositeSubscription subscriptions = new CompositeSubscription();
+
+    public BaseActivity(int layoutResID) {
+        this.layoutResID = layoutResID;
+    }
+
+    public IPresenter getPresenter() {
+        return presenter;
+    }
+
+    protected abstract IPresenter createPresenter(BaseActivity baseActivity, Bundle savedInstanceState);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layoutResID);
+        ButterKnife.inject(this);
+        this.onViewReady();
+        this.presenter = this.createPresenter(this, savedInstanceState);
+    }
+
+    protected void onViewReady()
+    {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.presenter.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.presenter.stop();
+        this.subscriptions.clear();
+    }
+}
