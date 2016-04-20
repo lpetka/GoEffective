1c71600b54f0efbabbb4ada254210fc1f024bbb5
maciej <maciewar@gmail.com>
2016-04-21 00:16:47 +0200

    Add day activity based on calendar

---
+++ app/src/main/java/project/io/goeffective/presenters/DayPresenter.java
@@ -0,0 +1,9 @@
+package project.io.goeffective.presenters;
+
+import project.io.goeffective.views.IDayView;
+
+public class DayPresenter extends Presenter<IDayView> {
+    public DayPresenter(IDayView iDayView) {
+        super(iDayView);
+    }
+}
