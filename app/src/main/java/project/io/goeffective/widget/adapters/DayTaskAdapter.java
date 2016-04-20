e6d9fbc7971ffad904a3802c5563b9d43bd2dee0
maciej <maciewar@gmail.com>
2016-04-21 01:58:09 +0200

    Display random tasks history

--- app/src/main/java/project/io/goeffective/widget/adapters/DayTaskAdapter.java
+++ app/src/main/java/project/io/goeffective/widget/adapters/DayTaskAdapter.java
@@ -1,14 +1,15 @@
 package project.io.goeffective.widget.adapters;
 
 import android.content.Context;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
+import project.io.goeffective.R;
 import project.io.goeffective.models.IDayModel;
 import project.io.goeffective.models.Task;
 
@@ -43,14 +44,45 @@ public class DayTaskAdapter extends BaseAdapter {
         return createListItem(task);
     }
 
-    private TextView createListItem(Task task) {
+    private View createListItem(Task task) {
+        RelativeLayout layout = new RelativeLayout(context);
+
+        final String taskName = task.getName();
+        View taskNameView = createTaskNameView(taskName);
+        layout.addView(taskNameView);
+
+        final List<Boolean> taskHistory = task.getHistory();
+        View taskProgressView = createTaskProgressView(taskHistory);
+        layout.addView(taskProgressView);
+
+        return layout;
+    }
+
+    private View createTaskNameView(String taskName) {
         TextView textView = new TextView(context);
-        textView.setText(task.getName());
-        textView.setGravity(Gravity.START);
+        textView.setText(taskName);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+        textView.setLayoutParams(params);
+        return textView;
+    }
+
+    private View createTaskProgressView(List<Boolean> taskHistory) {
+        TextView textView = new TextView(context);
+        final String historyLengthLabel = Integer.toString(taskHistory.size());
+        textView.setText(historyLengthLabel);
+
+        final int color = context.getResources().getColor(R.color.taskDone);
         textView.setHeight(70);
         textView.setTextSize(20);
-//        final int color = context.getResources().getColor(R.color.taskDone);
-//        textView.setBackgroundColor(color);
+        textView.setBackgroundColor(color);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        textView.setLayoutParams(params);
         return textView;
     }
 }
