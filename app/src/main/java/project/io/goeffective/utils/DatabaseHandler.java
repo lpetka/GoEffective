cfc153193b03ecfd4fceb146fdcc8cfe3e585218
Rafał Pachołek <rpachole@student.agh.edu.pl>
2016-05-09 19:49:51 +0200

    Created database handler

---
+++ app/src/main/java/project/io/goeffective/utils/DatabaseHandler.java
@@ -0,0 +1,25 @@
+package project.io.goeffective.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "GoEffective";
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
