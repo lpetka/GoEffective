d3a1aa072bc10e5b5b1750bd434a6b91cffa6691
Rafał Pachołek <rpachole@student.agh.edu.pl>
2016-05-09 20:19:13 +0200

    Create task table onCreate event

--- app/src/main/java/project/io/goeffective/utils/DatabaseHandler.java
+++ app/src/main/java/project/io/goeffective/utils/DatabaseHandler.java
@@ -9,13 +9,38 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "GoEffective";
 
+    private static final String TABLE_TASK = "tasks";
+    private static final String TABLE_TASK_START = "task_starts";
+    private static final String TABLE_TASK_DONE = "task_done";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_TASK_ID = "task_id";
+
+    //Table tasks
+    private static final String KEY_NAME = "name";
+
+    //Table task_starts
+    private static final String KEY_START = "start";
+    private static final String KEY_DELAY = "delay";
+
+    //Table task_done
+    private static final String KEY_DATE = "date";
+    private static final String KEY_COMMENT = "comment";
+
     public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    private void createTaskTable(SQLiteDatabase db){
+        String CREATE_TASK_TABLE = "CREATE TABLE " + TABLE_TASK + "("
+                + KEY_ID + " INTEGER PRIMARY KEY, "
+                + KEY_NAME + " TEXT" + ")";
+        db.execSQL(CREATE_TASK_TABLE);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        createTaskTable(sqLiteDatabase);
     }
 
     @Override
