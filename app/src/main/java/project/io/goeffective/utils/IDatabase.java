11bf57c2b98eb6a4b0eb8f6734fb3e08c1cf8199
Rafał Pachołek <rpachole@student.agh.edu.pl>
2016-05-15 00:17:26 +0200

    Changed method changeTask to updateTask.

--- app/src/main/java/project/io/goeffective/utils/IDatabase.java
+++ app/src/main/java/project/io/goeffective/utils/IDatabase.java
@@ -10,7 +10,7 @@ import project.io.goeffective.utils.dbobjects.Task;
 public interface IDatabase {
     void addTask(Task task);
     void removeTask(Task task);
-    void changeTask(Task task);
+    void updateTask(Task task);
     List<Pair<Task, Boolean>> getTasksStatusAtDate(Date date);
     List<Task> getTasksAtDate(Date date);
     void setTaskStatusAtDate(Date date, Task task, Boolean flag);
