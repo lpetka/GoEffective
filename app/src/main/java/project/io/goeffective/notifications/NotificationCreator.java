47d2e12397e1618b6a37ddb3aee2a62c489b583e
maciej <maciewar@gmail.com>
2016-05-14 17:18:01 +0200

    Add backward capability to notification builder

--- app/src/main/java/project/io/goeffective/notifications/NotificationCreator.java
+++ app/src/main/java/project/io/goeffective/notifications/NotificationCreator.java
@@ -1,9 +1,8 @@
 package project.io.goeffective.notifications;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.content.Context;
-import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 import project.io.goeffective.R;
 
@@ -14,9 +13,8 @@ public class NotificationCreator {
         this.context = context;
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public Notification createNotification(String title, String content) {
-        Notification.Builder builder = new Notification.Builder(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
         builder.setContentTitle(title);
         builder.setContentText(content);
         builder.setSmallIcon(R.drawable.day_task_not_done);
