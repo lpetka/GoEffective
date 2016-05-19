e4278615624f73a9216e5e8252ff23151f412e13
Łukasz Petka <petka@student.agh.edu.pl>
2016-05-19 03:10:08 +0200

    Add Task List Presenter

---
+++ app/src/main/java/project/io/goeffective/presenters/TasksListPresenter.java
@@ -0,0 +1,32 @@
+package project.io.goeffective.presenters;
+
+import project.io.goeffective.services.INavigator;
+import project.io.goeffective.views.ITasksListView;
+import rx.Scheduler;
+
+public class TasksListPresenter extends Presenter<ITasksListView> {
+    private Scheduler uiThread;
+    private final INavigator navigator;
+
+    public TasksListPresenter(ITasksListView iTasksListView, INavigator navigator, Scheduler uiThread) {
+        super(iTasksListView);
+        this.navigator = navigator;
+        this.uiThread = uiThread;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        this.subscriptions.add(view.addTaskClick().observeOn(uiThread).subscribe(o ->
+            {
+                navigator.openAddTaskActivity();
+            }
+        ));
+
+        this.subscriptions.add(view.addDummyTaskClick().observeOn(uiThread).subscribe(o ->
+            {
+                view.addDummyTask();
+            }
+        ));
+    }
+}
