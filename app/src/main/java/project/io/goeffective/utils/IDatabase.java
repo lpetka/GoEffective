719656008c0bef49cefd6acb177403dc2044b446
Łukasz Petka <petka@student.agh.edu.pl>
2016-05-19 03:04:58 +0200

    add getTasksList functionality

--- app/src/main/java/project/io/goeffective/utils/IDatabase.java
+++ app/src/main/java/project/io/goeffective/utils/IDatabase.java
@@ -14,4 +14,5 @@ public interface IDatabase {
     List<Pair<Task, Boolean>> getTasksStatusAtDate(Date date);
     List<Task> getTasksAtDate(Date date);
     void setTaskStatusAtDate(Date date, Task task, Boolean flag);
+    List<Task> getTasksList();
 }
