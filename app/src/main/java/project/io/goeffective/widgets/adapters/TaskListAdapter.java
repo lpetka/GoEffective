127615f6262ad56b56b5948a1e8f2ee87ad1353e
Łukasz Petka <petka@student.agh.edu.pl>
2016-05-31 13:05:35 +0200

    changes in TaskListAdapter

--- app/src/main/java/project/io/goeffective/widgets/adapters/TaskListAdapter.java
+++ app/src/main/java/project/io/goeffective/widgets/adapters/TaskListAdapter.java
@@ -12,19 +12,23 @@ import android.widget.Toast;
 import java.util.List;
 
 import butterknife.OnClick;
+import dagger.Module;
+import dagger.Provides;
 import project.io.goeffective.R;
 import project.io.goeffective.models.ITaskListModel;
 import project.io.goeffective.utils.dbobjects.Task;
 
 public class TaskListAdapter extends BaseAdapter {
-    private final List<Task> taskList;
+    private List<Task> taskList;
     private final Context context;
+    private final ITaskListModel taskListModel;
     private LayoutInflater inflater;
 
 
     public TaskListAdapter(Context context, ITaskListModel taskListModel) {
         this.context = context;
-        this.taskList = taskListModel.getDetailedTaskList();
+        this.taskListModel = taskListModel;
+        updateView();
     }
 
     @Override
@@ -50,14 +54,14 @@ public class TaskListAdapter extends BaseAdapter {
             convertView = inflater.inflate(R.layout.fragment_task_list_row, null);
         }
         TextView name = (TextView) convertView.findViewById(R.id.taskName);
-        Button editButton = (Button) convertView.findViewById(R.id.editTaskButton);
         Task task = taskList.get(position);
-        name.setText(task.getName());
+        String taskName = "Nazwa: " + task.getName() + " ID: " + task.getId();
+        name.setText(taskName);
+        return convertView;
+    }
 
-        editButton.setOnClickListener(
-                view -> Toast.makeText(context,"edit: "+task.getName(),Toast.LENGTH_SHORT).show()
-        );
-
-        return null;
+    public void updateView() {
+        taskList = taskListModel.getDetailedTaskList();
+        notifyDataSetChanged();
     }
 }
