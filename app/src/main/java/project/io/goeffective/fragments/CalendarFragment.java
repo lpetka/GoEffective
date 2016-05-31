84962b1dbc1f6a858c38ba78e9f001128d4d2150
Łukasz Petka <petka@student.agh.edu.pl>
2016-05-31 12:49:54 +0200

    move injecting view from superclass

--- app/src/main/java/project/io/goeffective/fragments/CalendarFragment.java
+++ app/src/main/java/project/io/goeffective/fragments/CalendarFragment.java
@@ -1,6 +1,12 @@
 package project.io.goeffective.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
 import project.io.goeffective.R;
 import project.io.goeffective.common.BaseFragment;
 import project.io.goeffective.presenters.CalendarPresenter;
@@ -11,8 +17,14 @@ import project.io.goeffective.views.ICalendarView;
 public class CalendarFragment extends BaseFragment implements ICalendarView {
 
     public CalendarFragment() {
-        args.putInt(StringConstants.FRAGMENT_RESOURCE_ID, R.layout.fragment_calendar);
-        super.setArguments(args);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
