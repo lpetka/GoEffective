c566f96a905ef36efac2c3ee887ef4413b980044
Łukasz Petka <petka@student.agh.edu.pl>
2016-06-01 15:51:48 +0200

    refactor AddTaskPresenter

--- app/src/main/java/project/io/goeffective/presenters/AddTaskPresenter.java
+++ app/src/main/java/project/io/goeffective/presenters/AddTaskPresenter.java
@@ -1,9 +1,110 @@
 package project.io.goeffective.presenters;
 
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.inject.Inject;
+
+import project.io.goeffective.App;
+import project.io.goeffective.activities.TaskAddActivity;
+import project.io.goeffective.services.INavigator;
+import project.io.goeffective.utils.DatabaseHandler;
+import project.io.goeffective.utils.dbobjects.Task;
+import project.io.goeffective.utils.dbobjects.TaskStart;
 import project.io.goeffective.views.IAddTaskView;
+import project.io.goeffective.widgets.WeekDayView;
+import project.io.goeffective.widgets.adapters.WeekDayListAdapter;
+import rx.Scheduler;
 
 public class AddTaskPresenter extends Presenter<IAddTaskView> {
-    public AddTaskPresenter(IAddTaskView iAddTaskView) {
+    private final Scheduler uiThread;
+    private final INavigator navigator;
+    private WeekDayView weekDayListSelector;
+    private final Context context;
+
+    @Inject
+    DatabaseHandler databaseHandler;
+
+    public AddTaskPresenter(IAddTaskView iAddTaskView, INavigator navigator, Scheduler uiThread, TaskAddActivity taskAddActivity) {
         super(iAddTaskView);
+        this.navigator = navigator;
+        this.uiThread = uiThread;
+        context = taskAddActivity;
+        App.getComponent().inject(this);
+    }
+
+    @Override
+    public void start() {
+        super.start();
+
+        this.subscriptions.add(view.createTaskButtonClick().observeOn(uiThread).subscribe(o -> {
+            if(weekDayListSelector == null) {
+                weekDayListSelector = view.getWeekDayListSelector();
+            }
+            createTask(view.getTaskName(), weekDayListSelector, view.getNoteInput());
+        }));
+    }
+
+    private void createTask(EditText taskName, WeekDayView weekDayListSelector, EditText noteInput) {
+        switch(validateData(taskName, weekDayListSelector)) {
+            case 0:     //valid
+                addTask(taskName, weekDayListSelector, noteInput);
+                break;
+            case 1:     //empty task name
+                Toast.makeText(context, "Podaj nazwę zadania.", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:     //no checked days
+                Toast.makeText(context, "Wybierz dni tygodnia.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private void addTask(EditText taskName, WeekDayView weekDayListSelector, EditText noteInput) {
+        Task task = new Task(taskName.getText().toString());
+        WeekDayListAdapter weekDayListAdapter = (WeekDayListAdapter) weekDayListSelector.getAdapter();
+        boolean[] checked = weekDayListAdapter.getChecked();
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0; i < checked.length; i++) {
+            if(checked[i])
+                sb.append(i).append(" ");
+        }
+        Toast.makeText(context, sb.toString(),Toast.LENGTH_SHORT).show();
+        GregorianCalendar date;
+        for(int i = 0; i < checked.length; i++) {
+            if(checked[i]) {
+                //in Gregorian Calendar day number 1 is Sunday
+                int myDay = (i + 2) % 7;
+                date = new GregorianCalendar(
+                        Calendar.getInstance().get(Calendar.YEAR),
+                        Calendar.getInstance().get(Calendar.MONTH),
+                        Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                );
+                while (date.get(Calendar.DAY_OF_WEEK) != myDay)
+                    date.add(Calendar.DATE, 1);
+                task.addTaskStart(new TaskStart(-1, date.getTime(), 7));
+            }
+        }
+        //task.setNote(noteInput.getText().toString());
+        databaseHandler.addTask(task);
+    }
+
+    private int validateData(EditText taskName, WeekDayView weekDayListSelector) {
+        if(taskName.getText().toString().matches(""))
+            return 1;
+
+        WeekDayListAdapter weekDayListAdapter = (WeekDayListAdapter) weekDayListSelector.getAdapter();
+        boolean[] checked = weekDayListAdapter.getChecked();
+        int i = 0;
+        while(i < checked.length) {
+            if (checked[i])
+                return 0;
+            i++;
+        }
+
+        return 2;
     }
 }
