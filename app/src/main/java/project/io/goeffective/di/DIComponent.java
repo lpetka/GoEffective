42661d6d4902bbe15401719d128a2482f27c8a0d
maciej <maciewar@gmail.com>
2016-06-01 17:10:38 +0200

    Add calendar model to di component

--- app/src/main/java/project/io/goeffective/di/DIComponent.java
+++ app/src/main/java/project/io/goeffective/di/DIComponent.java
@@ -5,6 +5,7 @@ import dagger.Component;
 import project.io.goeffective.App;
 import project.io.goeffective.activities.TaskEditActivity;
 import project.io.goeffective.fragments.TasksListFragment;
+import project.io.goeffective.models.CalendarModel;
 import project.io.goeffective.models.TaskListModel;
 import project.io.goeffective.presenters.AddTaskPresenter;
 import project.io.goeffective.presenters.PreferencesPresenter;
@@ -22,6 +23,7 @@ public interface DIComponent {
     void inject(App app);
     void inject(TaskListModel taskListModel);
     void inject(TasksListFragment    tasksListFragment);
+    void inject(CalendarModel calendarModel);
     void inject(TaskEditActivity taskEditActivity);
     void inject(AddTaskPresenter addTaskPresenter);
     void inject (TaskEditPresenter taskEditPresenter);
