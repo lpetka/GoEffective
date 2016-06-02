b16a12ed14bafea74b319fde29a2cb8a2bfcd5c7
maciej <maciewar@gmail.com>
2016-06-02 12:59:53 +0200

    Create notification updater

---
+++ app/src/main/java/project/io/goeffective/notifications/NotificationsUpdater.java
@@ -0,0 +1,48 @@
+package project.io.goeffective.notifications;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.util.Pair;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import project.io.goeffective.App;
+import project.io.goeffective.utils.DatabaseHandler;
+import project.io.goeffective.utils.dbobjects.Task;
+
+public class NotificationsUpdater {
+    @Inject
+    DatabaseHandler databaseHandler;
+
+    private final NotificationManager notificationManager;
+    private final NotificationScheduler notificationScheduler;
+    private final TaskNotificationCreator taskNotificationCreator;
+
+    public NotificationsUpdater(Context context) {
+        App.getComponent().inject(this);
+        final Object systemService = context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) systemService;
+        taskNotificationCreator = new TaskNotificationCreator(context);
+        notificationScheduler = new NotificationScheduler(context);
+    }
+
+    public void updateNotification() {
+        final Date currentDate = Calendar.getInstance().getTime();
+        final Pair<Date, List<Task>> nextDateTasksPair = databaseHandler.getNextNonemptyDayTasks(currentDate);
+        if (!nextDateTasksPair.second.isEmpty()) {
+            final Notification notification = taskNotificationCreator.createNotification(nextDateTasksPair.second);
+            notificationScheduler.cancelNotification();
+            if (nextDateTasksPair.first.getDate() == currentDate.getDate()) {
+                notificationManager.notify(TaskNotificationCreator.NOTIFICATION_ID, notification);
+            } else {
+                notificationManager.cancel(TaskNotificationCreator.NOTIFICATION_ID);
+                notificationScheduler.scheduleNotification(notification, nextDateTasksPair.first);
+            }
+        }
+    }
+}
